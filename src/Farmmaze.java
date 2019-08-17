f2eb32898d4e8a15a116b2eb1d9ab27bb3eed04a
Mathisfun123 <sairajareddykurelli@gmail.com>
2019-08-16 22:44:57 -0500

    Working on FarmMaze:

--- src/Farmmaze.java
+++ src/Farmmaze.java
@@ -10,12 +10,24 @@ public class Farmmaze {
         TreeMap<Integer, String> masterySet = new TreeMap<>();
         Scanner sc = new Scanner(new File("farmmaze.in"));
         while(sc.hasNextLine()){
-            String ln = sc.nextLine(); 
+            String ln = sc.nextLine();
             int year = Integer.parseInt(ln.split(" ")[0]);
-            masterySet.put(year, ln.substring(ln.indexOf(" ") +1));
+            masterySet.put(0, ln.substring(ln.indexOf(" ") +1));
         }
         System.out.println(masterySet);
 
 
     }
+    public class Fact {
+        public int year;
+        public int exposure;
+        public int correct;
+        public String fact;
+        public Fact(String s){
+            year = Integer.parseInt(s.split(" ")[0]);
+            exposure= 0;
+        }
+
+    }
+
 }
