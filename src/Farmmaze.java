131bf0196f4f12f662aa690aa3af0c93f4495397
Mathisfun123 <sairajareddykurelli@gmail.com>
2019-08-16 23:53:37 -0500

    Working on FarmMaze:

--- src/Farmmaze.java
+++ src/Farmmaze.java
@@ -1,30 +1,42 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Farmmaze {
     public static void main(String[] args) throws FileNotFoundException {
-        TreeMap<Integer, String> masterySet = new TreeMap<>();
+        TreeMap<Integer, Fact> masterySet = new TreeMap<>();
         Scanner sc = new Scanner(new File("farmmaze.in"));
         while(sc.hasNextLine()){
             String ln = sc.nextLine();
-            int year = Integer.parseInt(ln.split(" ")[0]);
-            masterySet.put(0, ln.substring(ln.indexOf(" ") +1));
+            masterySet.put(0, new Fact(ln));
+        }
+        // First Number in TreeMap is equal to exposure
+        Scanner sc2 = new Scanner(System.in);
+        for(int i = 0; i< 10; i++){
+            Map.Entry<Integer, Fact> t = masterySet.pollFirstEntry();
+            System.out.println(t.getValue().fact);
+            if(Integer.parseInt(sc2.nextLine())== (t.getKey())){
+                t.getValue().correct++;
+                t.getValue().exposure++;
+            }else{
+                t.getValue().exposure++;
+            }
+            masterySet.put(t.getValue().exposure, t.getValue());
         }
         System.out.println(masterySet);
 
 
     }
-    public class Fact {
+    public static class Fact {
         public int year;
         public int exposure;
         public int correct;
         public String fact;
         public Fact(String s){
             year = Integer.parseInt(s.split(" ")[0]);
+            fact = s.substring(s.indexOf(" ") +1);
             exposure= 0;
         }
 
